5edb593fa18f3988601e2da01ac908b95b2851fd
jeffrey chen <jeffreychen2013@gmail.com>
2013-06-20 01:01:18 -0700

    just finish the split up files part

--- src/fileutil/FileUtil.java
+++ src/fileutil/FileUtil.java
@@ -1,13 +1,8 @@
 package fileutil;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.OpenOption;
 import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
 
