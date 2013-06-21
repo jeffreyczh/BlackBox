96a2e2c1a7be4687a2e3a1ddcdadc3893b20b62a
jeffrey chen <jeffreychen2013@gmail.com>
2013-06-21 02:50:18 -0700

    add new function: watch the status of all files under a given folder

--- src/blackbox/TestDriver.java
+++ src/blackbox/TestDriver.java
@@ -4,6 +4,8 @@
  */
 package blackbox;
 
+import fileutil.FileUtil;
+
 /**
  * 
  * @author 
@@ -14,6 +16,6 @@ public class TestDriver {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       FileUtil.watchFiles("D:\\test");
     }
 }
--- src/fileutil/FileUtil.java
+++ src/fileutil/FileUtil.java
@@ -3,8 +3,19 @@ package fileutil;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.FileVisitResult;
+import java.nio.file.FileVisitor;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.StandardWatchEventKinds;
+import java.nio.file.WatchEvent;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.HashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -27,13 +38,11 @@ public class FileUtil {
          */
 	public static byte[][] splitFile(String path) {
             byte[][] chunks = null;
-            long fileSize = 0; // total bytes of the file
             int chunksCount = 0; // how many chunks the file can be splited up to
-            File file;
+            File file = new File(path);
+            long fileSize = file.length(); // total bytes of the file
             FileInputStream fileIs;
             try {
-                file = new File(path);
-                fileSize = file.length();
                 fileIs = new FileInputStream(file);
             } catch (IOException ex) {
                 System.out.println("IOException: fail to open inputstream for the file:" + path);
@@ -69,5 +78,75 @@ public class FileUtil {
             }
             return chunks;
 	}
-
+        /**
+         * watch the status of all files under the specified folder
+         * status includes creation, modification and deletion
+         * @param folderPath the path of the folder that is watched
+         */
+        public static void watchFiles(String folderPath) {
+            try {
+                // temporary put this function here, may change
+                final WatchService watchService = FileSystems.getDefault().newWatchService();
+                /*
+                 *  match the key with the path
+                 *  so that the full path of the folder which triggers events can be known
+                 */
+                final HashMap<WatchKey, Path> keyMap = new HashMap<>();
+                /* traverse the given folder and register the watch keys for all files in it */
+                registerWatchService(Paths.get(folderPath).toAbsolutePath(),
+                                     keyMap,
+                                     watchService);
+                while(true) {
+                    WatchKey key = watchService.take(); // wait for an event happends
+                    for (WatchEvent<?> event: key.pollEvents()) {
+                        Path tempFileFullPath = Paths.get(keyMap.get(key).toString(), event.context().toString());
+                        File tempFile = tempFileFullPath.toFile();
+                        if ( tempFile.isDirectory() ) {
+                            if ( event.kind().name().equals("ENTRY_CREATE") ) {
+                                /* if there is a new folder created, register for it */
+                                registerWatchService(tempFile.toPath(),
+                                                     keyMap,
+                                                     watchService);
+                                continue;
+                            }
+                            if ( event.kind().name().equals("ENTRY_MODIFY") ) {
+                                /* ignore the folder modification event */
+                                continue;
+                            }
+                        }
+                        /* belows are how we gonna deal with each event */
+                        System.out.println(tempFileFullPath + " has: " + event.kind());
+                    }
+                    key.reset();
+                }
+            } catch (IOException ex) {
+                System.out.println("IOException: fail to traverse the folder:" + folderPath);
+            } catch (InterruptedException ex) {
+                System.out.println("InterruptedException: fail to watch the folder:" + folderPath);
+            }
+        }
+        /**
+         * traverse the directory and register watch service for all files
+         * under this directoy and sub-directory
+         * @param dir the directory needs to be traversed
+         * @param keyMap the hashmap the matches the watch keys and paths
+         * @param watchService 
+         */
+        public static void registerWatchService( Path dir, 
+                                                    final HashMap<WatchKey, Path> keyMap,
+                                                    final WatchService watchService) throws IOException {
+            Files.walkFileTree(Paths.get(dir.toString()), 
+                new SimpleFileVisitor<Path>() {
+                    @Override
+                    public FileVisitResult preVisitDirectory(Path dir,
+                            BasicFileAttributes attrs) throws IOException {
+                       WatchKey key = dir.register(watchService, 
+                                          StandardWatchEventKinds.ENTRY_CREATE,
+                                          StandardWatchEventKinds.ENTRY_MODIFY,
+                                          StandardWatchEventKinds.ENTRY_DELETE);
+                        keyMap.put(key, dir);
+                        return FileVisitResult.CONTINUE;
+                    }
+                });
+        }
 }
